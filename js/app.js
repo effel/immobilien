73d359934b097d9e5d852f44cb5e82b5c2e36cdf
effel <rijik1@list.ru>
2017-09-09 02:24:35 +0300

    started work with items styles

--- js/app.js
+++ js/app.js
@@ -1,12 +1,5 @@
 var myApp = angular.module('myApp',  ['ngResource']);
 
-myApp.factory('getProperties', ['$resource', function($resource) {
-	return $resource('/json/properties.json', {
-	}, {
-			'getData': { method: 'GET'}
-	});
-}]);
-
 myApp.controller('ItemsController', [
 	'$scope',
 	'getProperties',
@@ -16,6 +9,25 @@ myApp.controller('ItemsController', [
 		 getProperties,
 		 $http) {
 
-getProperties.getData();
+		  $scope.propertiesArr = getProperties.getData({}, function (response) {
+					console.log(response);
+			}, function(rejection) {
+				  console.log(rejection);
+			});
 
 }]);
+
+// custom angular factories
+myApp.factory('getProperties', ['$resource', function($resource) {
+	return $resource('/json/properties.json', {
+	}, {
+			'getData': { method: 'GET'}
+	});
+}]);
+
+// custom angular filters
+myApp.filter('setItemImage', function($filter) {
+	return function(itemUrl) {
+		return !itemUrl ? "" : itemUrl ;
+	}
+});
