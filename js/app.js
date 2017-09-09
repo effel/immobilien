ddf315c4112648859c01677818225a9987be6841
effel <rijik1@list.ru>
2017-09-09 10:02:58 +0300

    fixed new label view

--- js/app.js
+++ js/app.js
@@ -1,36 +1,45 @@
-var myApp = angular.module('myApp',  ['ngResource','ui.router']);
+var myApp = angular.module('myApp',  ['ui.router','ngResource']);
 
 myApp.config(function($stateProvider,$urlRouterProvider){
 	$stateProvider.state('immobilien', {
 		url: '/immobilien',
 		templateUrl: 'index.html',
-		controller: 'ItemsController',
-		resolve: {
-
-		}
+		controller: 'ItemsController'
 	});
 	$urlRouterProvider.when('', '/immobilien');
 });
 
 myApp.controller('ItemsController', [
 	'$scope',
-	'getProperties',
-	'$http',
+	'GetProperties',
 	 function(
 		 $scope,
-		 getProperties,
-		 $http) {
+		 GetProperties,
+		) {
 
-		  $scope.propertiesArr = getProperties.getData({}, function (response) {
-					console.log(response);
-			}, function(rejection) {
-				  console.log(rejection);
-			});
+		$scope.propertiesArr = GetProperties.getData({}, function (response) {
+			console.log(response);
+		}, function(rejection) {
+			console.log(rejection);
+		});
 
+		$scope.setVisibleByDay = function(item) {
+			 const newDayCounter = 3;
+			 var currentDate = Date.now();
+			 var itemDate = new Date(item.meta.creation.date);
+			 var checkinDate = new Date(currentDate - itemDate).getDay();
+
+			 if (checkinDate > newDayCounter) {
+				return false; 
+			 } else {
+				 return true;
+			 }
+   
+		}
 }]);
 
 // custom angular factories
-myApp.factory('getProperties', ['$resource', function($resource) {
+myApp.factory('GetProperties', ['$resource', function($resource) {
 	return $resource('/json/properties.json', {
 	}, {
 			'getData': { method: 'GET'}
